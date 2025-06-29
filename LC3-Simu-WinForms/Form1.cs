49231add56c2c201c597eca6d3005c81520edff7
Nyahstic <nyahstic@gmail.com>
2025-06-29 16:45:22 -0300

    Yep, no more GUI.

--- LC3-Simu-WinForms/Form1.cs
+++
@@ -1,9 +0,0 @@
-namespace LC3_Simu_WinForms;
-
-public partial class Form1 : Form
-{
-    public Form1()
-    {
-        InitializeComponent();
-    }
-}
--- execute_wine.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd LC3-Simu-WinForms
-wine dotnet clean
-wine dotnet restore
-wine dotnet build --no-restore
-wine dotnet run --no-restore
-cd ..
