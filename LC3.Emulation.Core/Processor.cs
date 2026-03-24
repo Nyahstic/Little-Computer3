3d50e13912458f1b9ea092df87a80e2c48357e56
Nyahstic <themaximusthewolf@gmail.com>
2026-03-24 17:47:26 -0300

    Progress on the emulation!

--- LC3.Emulation.Core/Processor.cs
+++ LC3.Emulation.Core/Processor.cs
@@ -4,6 +4,20 @@ namespace LC3.Emulation.Core
 {
     public class Processor
     {
+        public Processor(ushort ProgramCouterStart = 0x3000)
+        {
+            RegisterFile[(int)Register.R_PC] = ProgramCouterStart; // Default starting address for the program counter
+            // for funsies
+            for (int i = 0; i < memory.Length; i++)
+            {
+                memory[i] = (ushort)Random.Shared.Next(0, ushort.MaxValue); // Fill memory with its own address for testing
+            }
+        }
+
+
+        // Event to signal debug information
+        public event Action<string>? OnDebugInfo;
+
         public bool Running = false;
 
         public enum Register{ 
@@ -18,10 +32,10 @@ namespace LC3.Emulation.Core
             R_COND, // R_COND = R_CONDITION
             R_COUNT
         };
-        public ushort[] Memory = new ushort[ushort.MaxValue + 1];
-        public ushort[] RegisterFile = new ushort[(int)Register.R_COUNT];
+        private ushort[] memory = new ushort[ushort.MaxValue + 1];
+        public ushort[] RegisterFile { get; private set; } = new ushort[(int)Register.R_COUNT];
         
-        public enum Opcodeenum
+        public enum OpCode : ushort
         {
             OP_BR = 0, /* branch */
             OP_ADD,    /* add  */
@@ -54,11 +68,144 @@ namespace LC3.Emulation.Core
         {
             RegisterFile[(int)Register.R_COND] = (ushort)Flags.FL_ZRO;
 
-            ushort instruction = Memory[RegisterFile[(int)Register.R_PC]++];
+
+            ushort instruction = MemoryRead(RegisterFile[(int)Register.R_PC]++);
 
             ushort opcode = (ushort)(instruction >> 12);
+            ushort r0, r1, r2, imm5, pcoffset9;
+            bool immFlag;
 
-            
+            r0 = (ushort)((instruction >> 9) & 0x7);
+            r1 = (ushort)((instruction >> 6) & 0x7);
+            r2 = (ushort)(instruction & 0x7);
+            immFlag = ((instruction >> 5) & 0x1) == 1;
+            imm5 = _signExtend((ushort)(instruction & 0x1F), 5);
+            pcoffset9 = _signExtend((ushort)(instruction & 0x1FF), 9);
+
+            //OnDebugInfo?.Invoke($"[DEBUG] Executing instruction at address: 0x{RegisterFile[(int)Register.R_PC]:X4}, Instruction: 0x{memory[RegisterFile[(int)Register.R_PC]]:X4}");
+            //OnDebugInfo?.Invoke($"[DEBUG] Decoded instruction - Opcode: {(OpCode)opcode}");
+
+            switch (opcode)
+            {
+                case (ushort)OpCode.OP_BR:
+                    throw new NotImplementedException($"please fix: {nameof(opcode)} got an yet-to-be implemented: OpCode.{(OpCode)opcode}");
+                    break;
+
+                case (ushort)OpCode.OP_ADD:
+                    if (immFlag)
+                    {
+                        RegisterFile[r0] = (ushort)(RegisterFile[r1] + imm5);
+                        OnDebugInfo?.Invoke($"[DEBUG] Executed OP_ADD with immediate value: R{r0} = R{r1} + {imm5} => 0x{RegisterFile[r0]:X4}");
+                    }
+                    else
+                    {
+                        RegisterFile[r0] = (ushort)(RegisterFile[r1] + RegisterFile[r2]);
+                        OnDebugInfo?.Invoke($"[DEBUG] Executed OP_ADD with register value: R{r0} = R{r1} + R{r2} => 0x{RegisterFile[r0]:X4}");
+                    }
+
+                    _updateFlags(r0);
+                    break;
+
+                case (ushort)OpCode.OP_LD:
+                    throw new NotImplementedException($"please fix: {nameof(opcode)} got an yet-to-be implemented: OpCode.{(OpCode)opcode}");
+                    break;
+
+                case (ushort)OpCode.OP_ST:
+                    throw new NotImplementedException($"please fix: {nameof(opcode)} got an yet-to-be implemented: OpCode.{(OpCode)opcode}");
+                    break;
+
+                case (ushort)OpCode.OP_JSR:
+                    throw new NotImplementedException($"please fix: {nameof(opcode)} got an yet-to-be implemented: OpCode.{(OpCode)opcode}");
+                    break;
+
+                case (ushort)OpCode.OP_AND:
+                    throw new NotImplementedException($"please fix: {nameof(opcode)} got an yet-to-be implemented: OpCode.{(OpCode)opcode}");
+                    break;
+
+                case (ushort)OpCode.OP_LDR:
+                    throw new NotImplementedException($"please fix: {nameof(opcode)} got an yet-to-be implemented: OpCode.{(OpCode)opcode}");
+                    break;
+
+                case (ushort)OpCode.OP_STR:
+                    throw new NotImplementedException($"please fix: {nameof(opcode)} got an yet-to-be implemented: OpCode.{(OpCode)opcode}");
+                    break;
+
+                case (ushort)OpCode.OP_RTI:
+                    throw new NotImplementedException($"please fix: {nameof(opcode)} got an yet-to-be implemented: OpCode.{(OpCode)opcode}");
+                    break;
+
+                case (ushort)OpCode.OP_NOT:
+                    throw new NotImplementedException($"please fix: {nameof(opcode)} got an yet-to-be implemented: OpCode.{(OpCode)opcode}");
+                    break;
+
+                case (ushort)OpCode.OP_LDI:
+                    OnDebugInfo?.Invoke($"[DEBUG] Executing OP_LDI, on Register {(Register)r0}, with offset of {pcoffset9}");
+                    RegisterFile[r0] = MemoryRead((ushort)(RegisterFile[(int)Register.R_PC] + pcoffset9));
+                    _updateFlags(r0);
+                    break;
+
+                case (ushort)OpCode.OP_STI:
+                    throw new NotImplementedException($"please fix: {nameof(opcode)} got an yet-to-be implemented: OpCode.{(OpCode)opcode}");
+                    break;
+
+                case (ushort)OpCode.OP_JMP:
+                    throw new NotImplementedException($"please fix: {nameof(opcode)} got an yet-to-be implemented: OpCode.{(OpCode)opcode}");
+                    break;
+
+                case (ushort)OpCode.OP_RES:
+                    throw new NotImplementedException($"please fix: {nameof(opcode)} got an yet-to-be implemented: OpCode.{(OpCode)opcode}");
+                    break;
+
+                case (ushort)OpCode.OP_LEA:
+                    throw new NotImplementedException($"please fix: {nameof(opcode)} got an yet-to-be implemented: OpCode.{(OpCode)opcode}");
+                    break;
+
+                case (ushort)OpCode.OP_TRAP:
+                    throw new NotImplementedException($"please fix: {nameof(opcode)} got an yet-to-be implemented: OpCode.{(OpCode)opcode}");
+                    break;
+
+                 default:
+                    throw new InvalidOperationException($"Unknown opcode: {opcode}");
+            }
+        }
+
+        private ushort MemoryRead(ushort Address)
+        {
+            return memory[Address % ushort.MaxValue];
+        }
+
+        public bool LoadImage(string path) {
+            if (!File.Exists(path))
+            {
+                OnDebugInfo?.Invoke($"[FILE] Image path does not exist: {path}");
+                return false;
+            }
+
+            return false;
+        }
+
+        private ushort _signExtend(ushort value, int bitCount)
+        {
+            if (((value >> (bitCount - 1)) & 1) == 1)
+            {
+                value |= (ushort)(0xFFFF << bitCount);
+            }
+            return value;
+        }
+
+        private void _updateFlags(ushort RegisterToCheck)
+        {
+            if (RegisterFile[RegisterToCheck] == 0) {
+                RegisterFile[(int)Register.R_COND] = (ushort)Flags.FL_ZRO;
+            } 
+            else if ((RegisterFile[RegisterToCheck] >> 15) != 0)
+            {
+                RegisterFile[(int)Register.R_COND] = (ushort)Flags.FL_NEG;
+            }
+            else
+            {
+                RegisterFile[(int)Register.R_COND] = (ushort)Flags.FL_POS;
+            }
         }
     }
 }   
---
+++ LC3.Emulation.TestSystem/Program.cs
@@ -0,0 +1,22 @@
+﻿using LC3.Emulation.Core;
+
+namespace LC3.Emulation.TestSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var proc = new Processor();
+            proc.OnDebugInfo += (info) => Console.WriteLine(info);
+            while (true)
+            {
+                try
+                {
+                    proc.Step();
+                } catch {
+                    continue;
+                }
+            }
+        }
+    }
+}
